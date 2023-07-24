40efea01a9c888125b0d4706a059f4f33f60d1fd
Vignesh Shetty <67872182+vign2000@users.noreply.github.com>
2023-07-24 18:48:00 +0530

    Update streamlit_app.py
    created variable fruit_choice and called it

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,11 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to dsiplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text('What fruit would you like information about?','Kiwi') #variable created
+stremlit.write('The user entered',fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #calling through variable
 
 
 # take the json version of response and normalize it
