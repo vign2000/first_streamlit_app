0d646035383164e1997fff63adee1560f29d25d0
Vignesh Shetty <67872182+vign2000@users.noreply.github.com>
2023-07-24 18:50:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to dsiplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')# variable created
+fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')# variable created
 stremlit.write('The user entered',fruit_choice)
 
 import requests
