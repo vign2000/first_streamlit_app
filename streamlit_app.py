95e691706652878b57430a4fda0e3a1b46d35756
Vignesh Shetty <67872182+vign2000@users.noreply.github.com>
2023-07-28 17:55:46 +0530

    Update streamlit_app.py
    added try catch

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,17 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to dsiplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')# variable created
-streamlit.write('The user entered',fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')# variable created
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #calling through variable
+    fruityvise_normalized = pandas.json_normalize(fruityvice_response.json()) # take the json version of response and normalize it
+    streamlit.dataframe(fruityvise_normalized) #output of the json file in table format
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #calling through variable
-
-# take the json version of response and normalize it
-fruityvise_normalized = pandas.json_normalize(fruityvice_response.json())
-#output of the json file in table format
-streamlit.dataframe(fruityvise_normalized)
+expect URLErroras e:
+  streamlit.error()
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
