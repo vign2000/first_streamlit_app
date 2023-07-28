557da2f2b4e6a785e1158df721763b2b2a19d43b
Vignesh Shetty <67872182+vign2000@users.noreply.github.com>
2023-07-28 18:17:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ except URLError as e:
 streamlit.header("The fruit load list contains:")
 #snowflake related funtions
 def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur():
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("slect * from fruit_load_list")
     return my_cur.fetchall()
 
